abc870d598f767a6f9856723d7656ebdf3117877
Le Cong Danh <congdanh5990@gmail.com>
2018-11-28 22:40:33 +0700

    complete section2

---
+++ Section2/v_for.js
@@ -0,0 +1,14 @@
+// Put this in the script section in JSFiddle
+// In a local setup, you need to merge this and the index.html file into one file
+new Vue({
+	el: '#app',
+	data: {
+        cars:['Honda','Yamaha','BMW','MAZDA'],
+        vegetable:['orange','apple','banana','coconut'],
+        persons:[
+            {name:'danh',age:27,add:'hcmct'},
+            {name:'tram',age:20,add:'bdpro'},
+            {name:'thao',age:15,add:'vtpro'},
+        ]
+	}
+})
